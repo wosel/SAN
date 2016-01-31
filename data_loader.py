287e78b1a516b1f1cdcdcafa35a825edb1ca81c9
Jakub Hajic <j.hajic@gmail.com>
2016-01-31 21:51:50 +0100

    DATA: text dataset loader full w/ sparse

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd;
 import numpy as np;
 import scipy.ndimage as spi
+from scipy.sparse import csc_matrix, csr_matrix
 import re
 import sys
 from os import listdir
@@ -10,7 +11,8 @@ class qa:
     def __init__(self):
         self.qID = -1
         self.question = []
-        self.oneHotWords = []
+        self.oneHotQuestion
+        self.oneHotAnswer
         self.imageID = -1
         self.answer = []
     def __init__(self, qID, wordList, imageID):
@@ -60,24 +62,34 @@ def loadFile(filename):
     tmpQuestion = []
     tmpImgID = -1
     wordSet = set()
-
+    maxAnswerLen = -1
+    maxQuestionLen = -1
     for line in open(filename, 'r'):
         allWords = line.split()
         for w in allWords:
             wordSet.add(w)
         if(allWords[-1]) != '?': # this is an answer
             tmpAnswer = allWords
+
             question = qa(qID=qID, wordList=tmpQuestion, answerList=tmpAnswer, imageID=tmpImgID)
             questionList = [question] + questionList
+            if len(allWords) > maxAnswerLen:
+                maxAnswerLen = len(allWords)
+
         else:
             qID += 1
             tmpQuestion = allWords[:-4]
+            tmpQuestion = tmpQuestion + ['?']
+
             imString = allWords[-2]
             imString = re.sub('image', '', imString)
             tmpImgID = int(imString)
-    return [questionList.reverse(), wordSet]
+            if (len(allWords) - 3) > maxQuestionLen:
+                maxQuestionLen = len(allWords) - 3
+    questionList.reverse()
+    return [questionList, wordSet, maxQuestionLen, maxAnswerLen]
 
-[questionList, fullWordSet] = loadFile(daquarFolder+'/qa.894.raw.txt')
+[qList, fullWordSet, maxQL, maxAL] = loadFile(daquarFolder + '/qa.37.raw.txt')
 
 print(len(fullWordSet))
 fullWordDict = {}
@@ -85,9 +97,32 @@ i = 0;
 for w in fullWordSet:
     fullWordDict[w] = i
     i+=1;
-print(fullWordDict['a'])
 
+print("maxAL: " + str(maxAL))
+print("maxQL: " + str(maxQL))
 
+for q in qList:
+    if (q.qID % 100 == 0): print(q.qID)
+    q.oneHotQuestion = csc_matrix((len(fullWordDict), maxQL), dtype=float)
+    q.oneHotAnswer = csc_matrix((len(fullWordDict), maxAL), dtype=float)
+
+    pos = 0
+    for w in q.question:
+        id = fullWordDict[w]
+        q.oneHotQuestion[id, pos] = 1.
+        pos += 1
+    pos = 0
+    for w in q.answer:
+        id = fullWordDict[w]
+        q.oneHotAnswer[id, pos] = 1.
+        pos += 1
+
+print (qList[0].oneHotQuestion)
+print (qList[0].oneHotQuestion.toarray())
+print (qList[0].question)
+print (qList[0].oneHotAnswer)
+print (qList[0].oneHotAnswer.toarray())
+print (qList[0].answer)
 
 
 
