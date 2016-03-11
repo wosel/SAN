2472d4627d69e108eda1318ad2423d2d4f3d0b63
Jakub Hajic <jakub@wks-12-32>
2016-03-11 01:17:24 +0100

    BIN: added script to run model.py

---
+++ runModel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while getopts "h?d:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "-d device"
+        exit 0
+        ;;
+    d)  device=$OPTARG
+        ;;
+    esac
+done
+
+echo $device
+
+if [[ $device == 'cpu' ]] || [[ $device =~ gpu[0123] ]] ; then
+ :
+else
+	echo "bad or no device";
+	exit 1;
+fi
+
+TF="device=$device,optimizer=fast_compile"
+
+export THEANO_FLAGS=$TF
+
+python model.py
+
